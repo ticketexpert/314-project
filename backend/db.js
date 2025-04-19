d232ba639c5fbe2db221124bba1323df74e6d240
Matthew <55415791+BergaDev@users.noreply.github.com>
2025-04-19 18:10:32 +1000

    Remote DB connection

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('eventdb', 'eventuser', 'eventpass', {
-  host: 'localhost',
+  host: '103.4.235.10',
+  port: 5432,
   dialect: 'postgres',
 });
 
