2e94d46f15e5a8d091a5ec81deeddabdbf5a7080
Khiem Nguyen <hkn215@uowmail.edu.au>
2025-05-07 21:32:36 +1000

    Fix Frontend CI

--- user/vitest.config.ts
+++ user/vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
+        coverage: {
+            reporter: ['text', 'lcov'],
+        },
       include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}', 'tests/**/*.{test,spec}.{js,ts,jsx,tsx}'],
     },
   });
