2201f36e4875857ca9672fcd00673e3f73a03ae4
Matthew <55415791+BergaDev@users.noreply.github.com>
2025-05-13 20:30:03 +1000

    creating testDB to save production data

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('eventdb', 'eventuser', 'eventpass', {
-  host: '103.4.235.10',
-  port: 5432,
-  dialect: 'postgres',
-});
+const isTest = process.env.NODE_ENV === 'test';
+
+const sequelize = new Sequelize(
+  isTest ? 'eventdb_test' : 'eventdb',
+  'eventuser',
+  'eventpass',
+  {
+    host: '103.4.235.10',
+    port: 5432,
+    dialect: 'postgres'
+  }
+);
 
 module.exports = sequelize;
