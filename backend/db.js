327d3576d819fdc69855b4329cb2f6bae445ce45
Matthew <55415791+BergaDev@users.noreply.github.com>
2025-05-19 18:30:49 +1000

    backend

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,15 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config({ path: '.db.env' });
 
 const isTest = process.env.NODE_ENV === 'test';
 
 const sequelize = new Sequelize(
-  isTest ? 'eventdb_test' : 'eventdb',
-  'eventuser',
-  'eventpass',
+  isTest ? process.env.DB_TEST_NAME : process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: '103.4.235.10',
-    port: 5432,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres'
   }
 );
