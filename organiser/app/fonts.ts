6e0c13eccc6b60d9c58bad52e6fae6194df7c7c3
Khiem Nguyen <hkn215@uowmail.edu.au>
2025-05-22 13:14:21 +1000

    change font on organiser

---
+++ organiser/app/fonts.ts
@@ -0,0 +1,8 @@
+import { Rethink_Sans } from 'next/font/google';
+
+export const rethinkSans = Rethink_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-rethink-sans',
+  display: 'swap',
+}); 
--- organiser/app/layout.tsx
+++ organiser/app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css';
+import { rethinkSans } from './fonts';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={rethinkSans.variable}>
+      <body className="font-sans">{children}</body>
     </html>
   );
 }
