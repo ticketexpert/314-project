dd74606ea621a5e0571a70775533b5e4ccc2b9f2
Matthew <55415791+BergaDev@users.noreply.github.com>
2025-05-22 11:12:11 +1000

    Update status.js

--- backend/routes/status.js
+++ backend/routes/status.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 
 // GET /status/version - Returns the version of the site
 router.get('/version', (req, res) => {
-    res.json({ version: 'Thur 22May 10:48am' });
+    res.json({ version: 'Thur 22May 11:00am' });
 });
 
 module.exports = router;
