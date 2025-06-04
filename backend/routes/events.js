26c6d6bf638754e9cc3d8e4e3255ed78782f6d58
Matthew <55415791+BergaDev@users.noreply.github.com>
2025-06-04 14:11:11 +1000

    Fix update notif

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -130,7 +130,7 @@ router.patch('/:eventId', async (req, res) => {
 
     try {
       await axios.put(`https://www.api.ticketexpert.me/api/userNotification/${attendee.userId}`, {
-        "currentNotifs": {"title": "Event Update", "message": "The event you are attending has been updated. here is what has changed: " + updateData}
+        "currentNotifs": {"title": "Event Update", "message": "The event you are attending has been updated. here is what has changed: " + req.body}
       });
     } catch (postError) {
       console.error('Error posting user notification:', postError);
