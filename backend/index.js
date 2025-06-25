7a5abf23b4af4c7d8084aae1aa4741e796fdc627
Matthew <55415791+BergaDev@users.noreply.github.com>
2025-06-25 10:12:03 +1000

    Change backend port setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const app = require('./app');
 const sequelize = require('./db');
+require('dotenv').config({ path: '.db.env' });
 
 sequelize.sync().then(() => {
   console.log('Database synced successfully');
-  app.listen(3020, '0.0.0.0', () => console.log('Server running on http://0.0.0.0:3020'));
+  app.listen(process.env.SERVER_PORT, '0.0.0.0', () => console.log(`Server running on http://0.0.0.0:${process.env.SERVER_PORT}`));
 }).catch(err => {
   console.error('Error syncing database:', err);
 });
